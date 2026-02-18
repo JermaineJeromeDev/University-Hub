0a0c8406f1f87db2a1425cec94de1bbebc06420c
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-02-18 14:06:34 +0100

    feat(admin): register core models

--- campus/admin.py
+++ campus/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Semester, Professor, Student, StudentIDCard, Course, CourseDescription
 
 # Register your models here.
+
+admin.site.register(Semester)
+admin.site.register(Professor)
+admin.site.register(Student)
+admin.site.register(StudentIDCard)
+admin.site.register(Course)
+admin.site.register(CourseDescription)
