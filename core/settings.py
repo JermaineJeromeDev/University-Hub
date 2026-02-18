28cc32b768ebb531e80d01d28c7b300dfcf5d5cc
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-02-18 13:32:11 +0100

    feat: register campus app and configure CORS/CSRF settings add 'campus' to INSTALLED_APPS implement CorsMiddleware and configure allowed origins set CSRF_TRUSTED_ORIGINS for frontend development (port 5500) enable CORS_ALLOW_ALL_ORIGINS for testing

--- core/settings.py
+++ core/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'campus'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +50,18 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:5500',
+    'http://localhost:5500',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:5500',
+    'http://localhost:5500',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'core.urls'
 
